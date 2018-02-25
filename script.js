93241d9bf8afe65df4036531a976ed1b855b87b6
Craig Dempsey <craigdempsey@gmail.com>
2018-02-25 02:59:18 -0500

    has a grid

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
-// make board tiles
+let cols = 9;
+let rows = 22;
 
+// make board tiles
+let board = document.getElementById('board');
+
+for (let j = 0; j<rows; j++) {
+    for (let i = 0; i<cols; i++) {
+        let classes = ["board-tile", `c-${i}`, `r-${j}`];
+        let tile = document.createElement('div');
+        tile.classList.add(...classes);
+        board.appendChild(tile);
+    }
+}
