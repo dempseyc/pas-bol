c78399d0729d24e20f09078b682ca43f880ebbd3
Craig Dempsey <craigdempsey@gmail.com>
2018-02-25 03:27:36 -0500

    takes keycodes

--- script.js
+++ script.js
@@ -12,3 +12,54 @@ for (let j = 0; j<rows; j++) {
         board.appendChild(tile);
     }
 }
+
+function slideBoardCells(direction) {
+    switch(direction) {
+        case "up":
+            console.log("up");
+            break;
+        case "down":
+            console.log("down");
+            break;
+        case "left":
+            console.log("left");
+            break;
+        case "right":
+            console.log("right");
+            break;
+        case "pass":
+            console.log("pass");
+            break;
+    }
+}
+
+document.addEventListener('keydown', function(event) {
+    
+      switch(event.which) {
+          case 37: // left arrow
+            slideBoardCells("right");
+            break;
+
+          case 38: // up arrow
+            slideBoardCells("up");
+            break;
+
+          case 39: // right arrow
+            slideBoardCells("left");
+            break;
+
+          case 40: // down arrow
+            slideBoardCells("down");
+            break;
+
+          case 88:
+          case 191:
+            slideBoardCells("pass");
+            break;
+
+          default: return;
+      }
+
+      event.preventDefault(); // prevent the default action (scroll / move caret)
+
+  }); //document keydown function
